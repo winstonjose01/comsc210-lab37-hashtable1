acc95f07f94aa2b82e5335c7830081f9363a5c7a
winstonjose01 <winston.jose@gmail.com>
2024-12-05 21:45:22 -0800

    completed PART TWO

--- lab-37-starter.cpp
+++ lab-37-starter.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int sumascii (const string&);
 
+
 int main() {
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
+    // char a = 'A';
+    // cout << a << endl;
+    // cout << (int) a << endl;
+    // int b = 66;
+    // cout << b << endl;
+    // cout << (char) b << endl;
 
-    int sum_char = sumascii("helloWORLD"); // for testing total ascii val should be 924
-    cout << "the sum is " << sum_char; // for testing
+    // ------------------ PART TWO --------------------------
+    string line;
+    int total = 0;
+    int sum_str = 0;
+    ifstream fin("lab-37-data.txt");
+
+    if (!fin.is_open()){
+        cout << "Error opening file. Aborting" << endl;
+        return(1);
+    }
+
+    while (getline(fin, line)){
+        sum_str = sumascii(line);
+        total += sum_str;
+    }
+    fin.clear();
+    fin.seekg(0);
+    cout <<"Total ASCII values in the entire file = " << total << endl;
+
+    // ----------------- END OF PART TWO ------------------
+
+    int part_one = sumascii("helloWORLD"); // for testing total ascii val should be 924
+    cout << "the ascii sum of helloWORLD = " << part_one; // for testing
     
-
     return 0;
 }
 
